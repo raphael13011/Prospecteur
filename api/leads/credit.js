2eeffbb84808031dbd55602ef95b43dd4be1c097
De Garam Raphael <raphael.degaram@gmail.com>
2026-09-14 18:41:28 +0200

    Create credit.js

---
+++ api/leads/credit.js
@@ -0,0 +1,16 @@
+const Credit = require("../../lib/models/credit");
+const { requireAuth, handleCors } = require("../../lib/auth");
+
+module.exports = async function handler(req, res) {
+  if (handleCors(req, res)) return;
+  if (req.method !== "GET") return res.status(405).json({ error: "Method not allowed" });
+
+  const user = await requireAuth(req, res);
+  if (!user) return;
+
+  const balance = await Credit.getBalance(Number(user.id));
+  const transactions = await Credit.getTransactions(Number(user.id), 20);
+  const packs = Credit.PACKS;
+
+  res.json({ balance, transactions, packs });
+};
